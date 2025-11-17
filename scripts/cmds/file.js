3a8b5e37e1c6278b19296e26fdf5aaa5341be7cf
goatbotnx <onlygoatbot.nx@gmail.com>
2025-11-17 10:10:18 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100081088184521", "61582662637419",];
+		const permission = ["100081088184521", "100049295749147",];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" khankir chele ja vaggg 🐤", event.threadID, event.messageID);
 		}
